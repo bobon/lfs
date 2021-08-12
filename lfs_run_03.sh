053a3a7595fb29819973157dae930769bd76c4f7
bobon <19086312@qq.com>
2021-08-13 07:13:44 +0800

    03

--- lfs_run_03.sh
+++ lfs_run_03.sh
@@ -367,7 +367,7 @@ make install
 install -v -dm755  /usr/share/doc/dejagnu-1.6.3
 install -v -m644   doc/dejagnu.{html,txt} /usr/share/doc/dejagnu-1.6.3
 # 如果要测试该软件包，执行：
-make check
+make check || true
 cd ..
 end_tool dejagnu 
 
